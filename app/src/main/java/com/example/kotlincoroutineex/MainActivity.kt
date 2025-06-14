e37e8cbc877c45c45ebb5da111753f7f33039d88
liumingsong <liumingsong@baijia.com>
2025-06-14 21:07:31 +0800

    [feat] 1.0、1.1-切协程: launch()

--- app/src/main/java/com/example/kotlincoroutineex/MainActivity.kt
+++ app/src/main/java/com/example/kotlincoroutineex/MainActivity.kt
@@ -1,33 +1,77 @@
 package com.example.kotlincoroutineex
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.View
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.kotlincoroutineex.ui.theme.KotlinCoroutineExTheme
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.newFixedThreadPoolContext
+import kotlinx.coroutines.newSingleThreadContext
+import java.util.concurrent.Executors
+import kotlin.concurrent.thread
+import kotlin.coroutines.EmptyCoroutineContext
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            KotlinCoroutineExTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+
+        Thread {
+
+        }.start()
+
+        thread {
+
         }
+
+        println("Main thread: ${Thread.currentThread().name}")
+
+        val executor = Executors.newCachedThreadPool()
+        executor.execute {
+            println("Executor thread: ${Thread.currentThread().name}")
+        }
+
+        val scope = CoroutineScope(Dispatchers.Default)
+        scope.launch(Dispatchers.Default) {
+            println("CoroutineScope thread: ${Thread.currentThread().name}")
+        }
+
+        val threadPoolContext = newFixedThreadPoolContext(10, "threadPool1")
+        scope.launch(threadPoolContext) {
+
+        }
+
+        val singleThreadContext = newSingleThreadContext("singleThread1")
+
+
+
+        val handler = Handler(Looper.getMainLooper())
+        handler.post {
+
+        }
+
+        val view = View(this)
+        view.post {
+
+        }
+
+
+        GlobalScope.launch {
+
+        }
+
+
     }
+
+
 }
 
 @Composable
