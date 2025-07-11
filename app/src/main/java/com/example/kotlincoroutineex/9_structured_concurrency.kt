987f82e684bc0e35ed6924912570e1f751c36c38
liumingsong <liumingsong@baijia.com>
2025-07-11 21:41:30 +0800

    [feat] 1.9-结构化并发初探

---
+++ app/src/main/java/com/example/kotlincoroutineex/9_structured_concurrency.kt
@@ -0,0 +1,59 @@
+package com.example.kotlincoroutineex
+
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.widget.TextView
+import androidx.activity.ComponentActivity
+import androidx.lifecycle.lifecycleScope
+import com.example.kotlincoroutineex.common.Contributor
+import com.example.kotlincoroutineex.common.gitHub
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import kotlin.concurrent.thread
+
+/**
+ * author: liumingsong
+ * created on: 2025/7/2 16:45
+ * description:
+ */
+class StructuredConcurrencyActivity : ComponentActivity() {
+    private lateinit var infoTextView: TextView
+
+    private var job: Job? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.layout_1)
+        infoTextView = findViewById(R.id.infoTextView)
+
+        val job = coroutinesStyle()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        job?.cancel()
+        lifecycleScope.cancel()
+    }
+
+    private fun coroutinesStyle() = lifecycleScope.launch {
+        val contributors = gitHub.contributors("square", "retrofit")
+        showContributors(contributors)
+    }
+
+    private suspend fun showContributorsOfRetrofit(): List<Contributor> {
+        return gitHub.contributors("square", "retrofit")
+    }
+
+    private fun showContributors(contributors: List<Contributor>) = contributors
+        .map { "${it.login} (${it.contributions})" }
+        .reduce { acc, s -> "$acc\n$s" }
+        .let { infoTextView.text = it }
+
+}
