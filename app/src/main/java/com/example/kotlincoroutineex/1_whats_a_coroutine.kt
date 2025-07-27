8eb4e4fcfecb1ea8c0c61c2aaa696fd07dbb79d9
liumingsong <liumingsong@baijia.com>
2025-07-27 09:51:29 +0800

    [feat] 2.1-概念落地：「一个协程」到底指的是什么

---
+++ app/src/main/java/com/example/kotlincoroutineex/1_whats_a_coroutine.kt
@@ -0,0 +1,57 @@
+package com.example.kotlincoroutineex
+
+import com.example.kotlincoroutineex.common.gitHub
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import kotlin.concurrent.thread
+import kotlin.coroutines.ContinuationInterceptor
+
+
+/**
+ * author: liumingsong
+ * created on: 2025/7/2 16:45
+ * description:
+ */
+fun main(): Unit = runBlocking {
+
+    val contributor = gitHub.contributors("square", "retrofit")
+
+    val scope = CoroutineScope(Dispatchers.IO)
+    var innerJob: Job? = null
+    var innerScope: CoroutineScope? = null
+    val outerJob = scope.launch(Dispatchers.Default) {
+        innerJob = coroutineContext[Job]
+        innerScope = this
+    }
+    println("outerJob: $outerJob")
+    println("innerJob: $innerJob")
+    println("outerJob === innerScope: ${outerJob === innerScope}")
+
+
+
+
+
+//    launch {
+//
+//    }
+//
+//    Thread {
+//
+//    }.start()
+//
+//    val thread = thread {
+//
+//    }
+//    thread.name
+//    thread.interrupt()
+
+
+
+
+}
+
+
+
