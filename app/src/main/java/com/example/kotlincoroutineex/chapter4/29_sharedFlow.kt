31c8851e0b511931bb8d4685f67a07e69478f4cd
liumingsong <liumingsong@baijia.com>
2026-02-02 15:49:44 +0800

    [feat] 4.29-SharedFlow 的效果和适用场景

---
+++ app/src/main/java/com/example/kotlincoroutineex/chapter4/29_sharedFlow.kt
@@ -0,0 +1,73 @@
+package com.example.kotlincoroutineex.chapter4
+
+
+import kotlinx.coroutines.CoroutineName
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.combineTransform
+import kotlinx.coroutines.flow.flatMapConcat
+import kotlinx.coroutines.flow.flatMapMerge
+import kotlinx.coroutines.flow.flattenConcat
+import kotlinx.coroutines.flow.flattenMerge
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.merge
+import kotlinx.coroutines.flow.retry
+import kotlinx.coroutines.flow.shareIn
+import kotlinx.coroutines.flow.zip
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.newFixedThreadPoolContext
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
+import kotlin.NullPointerException
+import kotlin.coroutines.EmptyCoroutineContext
+
+
+
+/**
+ * author: liumingsong
+ * created on: 2025/7/2 16:45
+ * description:
+ */
+fun main(): Unit = runBlocking {
+    val scope = CoroutineScope(EmptyCoroutineContext)
+    val flow1 = flow {
+        delay(500)
+        emit(1)
+        delay(500)
+        emit(2)
+        delay(500)
+        emit(3)
+    }
+
+    flow1.launchIn(scope)
+
+    val sharedFlow = flow1.shareIn(scope, SharingStarted.Eagerly)
+
+    scope.launch {
+        delay(1400)
+        sharedFlow.collect {
+            println("collect()-1: $it")
+        }
+    }
+
+    scope.launch {
+        delay(1400)
+        sharedFlow.collect {
+            println("collect()-2: $it")
+        }
+    }
+
+
+    delay(8000)
+}
+
+
