1b7c373a84062d0dd657354d1387c2df1687259d
Roxas0908 <2843634012@qq.com>
2026-03-29 15:59:52 +0800

    [feat] SupervisorJob 补充

--- app/src/main/java/com/example/kotlincoroutineex/chapter2/12_SupervisorJob.kt
+++ app/src/main/java/com/example/kotlincoroutineex/chapter2/12_SupervisorJob.kt
@@ -5,6 +5,7 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.cancel
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.job
 import kotlinx.coroutines.launch
@@ -20,16 +21,36 @@ import kotlin.coroutines.EmptyCoroutineContext
  * description:
  */
 fun main() = runBlocking {
-    val context = Dispatchers.IO + Job() + Job()
-    val scope = CoroutineScope(EmptyCoroutineContext)
+    // 1️⃣ 创建一个 SupervisorJob，这个 Job 会作为 scope 的父 Job
+    val supervisorJob = SupervisorJob()
 
-    val job = scope.launch {
-        this.coroutineContext[Job]
-        coroutineContext.job
-        coroutineContext[ContinuationInterceptor]
+    // 2️⃣ 创建一个 CoroutineScope，挂上 SupervisorJob
+    val scope = CoroutineScope(Dispatchers.Default + supervisorJob)
+
+
+    launch {
+        scope.launch {
+            // 子协程1：会抛异常
+            launch {
+                throw RuntimeException("子1挂了")
+            }
+        }
+        // 子协程2：延迟打印
+        launch {
+            delay(400)
+            println("子2还能跑")
+        }
+
+        // 父协程自己也延迟打印
+        delay(500)
+        println("父协程还能打印")
     }
 
-    delay(10000)
+
+    // 让 main 不立即退出
+    delay(1000)
+
+    println("scope.active = ${scope.coroutineContext[Job]?.isActive}")  // true
 }
 
 
